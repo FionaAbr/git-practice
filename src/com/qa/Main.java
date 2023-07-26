f8930f2689d214aae7c38d232e49e7209b2a9ea5
Fiona <fiona@db.com>
2023-07-26 08:41:01 +0100

    Initial commit

---
+++ src/com/qa/Main.java
@@ -0,0 +1,14 @@
+package com.qa;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+    Rectangle r = new Rectangle ("rectangle1", "yellow", 0, 0, 10, 10);
+    Rectangle r2 = new Rectangle ("rectangle2", "green", 2, 2, 5, 2);
+        System.out.println(r);
+        System.out.println(r2);
+
+
+
+
+}
